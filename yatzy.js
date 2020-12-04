fb6f4deacdc0a75a7ad5d68a432302e9dc59c0f7
SqueakyNinja <SqueakyNinja@users.noreply.github.com>
2020-12-04 12:03:15 +0100

    WIP set active player

--- yatzy.js
+++ yatzy.js
@@ -26,19 +26,45 @@ class Dice {
   }
 
   insertValue() {
-    let td1 = document.getElementById("td1");
+    let td1 = document.getElementById("ones1");
     td1.innerHTML = 5;
 
-    let td2 = document.getElementById("td2");
+    let td2 = document.getElementById("ones2");
     td2.innerHTML = 3;
   }
 }
-let dice1 = new Dice(1, "dice1");
-let dice2 = new Dice(2, "dice2");
-let dice3 = new Dice(3, "dice3");
-let dice4 = new Dice(4, "dice4");
-let dice5 = new Dice(5, "dice5");
-let dice6 = new Dice(6, "dice6");
+
+class Player {
+  constructor(id, name, setActivePlayer) {
+    this.id = id;
+    this.name = name;
+    this.setName();
+    this.setActivePlayer = setActivePlayer;
+  }
+  setName() {
+    document.getElementById("player" + this.id).innerHTML = this.name;
+  }
+}
+
+function nextPlayer() {
+  player(activePlayer).setActivePlayer = true;
+}
+function init() {
+  let player1 = new Player(1, "Göran", false);
+  let player2 = new Player(2, "Bengt", false);
+  let player3 = new Player(3, "Gunhild", false);
+  let player4 = new Player(4, "Sven-Bertil", false);
+
+  let dice1 = new Dice(1, "dice1");
+  let dice2 = new Dice(2, "dice2");
+  let dice3 = new Dice(3, "dice3");
+  let dice4 = new Dice(4, "dice4");
+  let dice5 = new Dice(5, "dice5");
+  let dice6 = new Dice(6, "dice6");
+  let activePlayer = 1;
+  nextPlayer();
+}
+
 function rollAllDice() {
   dice1.rollDice();
   dice2.rollDice();
@@ -48,4 +74,4 @@ function rollAllDice() {
   dice6.rollDice();
 }
 
-dice1.insertValue();
+init();
