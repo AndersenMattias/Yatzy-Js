c603da97c786d3d181787f2d3d9dedf4a3f02b59
SqueakyNinja <SqueakyNinja@users.noreply.github.com>
2020-12-04 10:39:13 +0100

    now you can roll all dice

--- yatzy.js
+++ yatzy.js
@@ -1,3 +1,26 @@
-const randomDiceNumber = () =>
-  (document.getElementById("dice1").src =
-    "dice" + Math.floor(Math.random() * 6 + 1) + ".png");
+class Dice {
+  constructor(value, id) {
+    this.value = value;
+    this.id = document.getElementById(id);
+  }
+  rollDice() {
+    this.id.src = "dice" + this.random() + ".png";
+  }
+  random() {
+    return (this.value = Math.floor(Math.random() * 6 + 1));
+  }
+}
+let dice1 = new Dice(1, "dice1");
+let dice2 = new Dice(2, "dice2");
+let dice3 = new Dice(3, "dice3");
+let dice4 = new Dice(4, "dice4");
+let dice5 = new Dice(5, "dice5");
+let dice6 = new Dice(6, "dice6");
+function rollAllDice() {
+  dice1.rollDice();
+  dice2.rollDice();
+  dice3.rollDice();
+  dice4.rollDice();
+  dice5.rollDice();
+  dice6.rollDice();
+}
