8f534e6f9496a86b0b67aa2b9ee640b9f6e8ae30
SqueakyNinja <SqueakyNinja@users.noreply.github.com>
2020-12-07 11:55:57 +0100

    fixed sum

--- yatzy.js
+++ yatzy.js
@@ -35,33 +35,69 @@ class Dice {
 }
 
 class Player {
-  constructor(id, name, setActivePlayer) {
+  constructor(id, name) {
     this.id = id;
     this.name = name;
     this.setName();
-    this.setActivePlayer = setActivePlayer;
   }
   setName() {
     document.getElementById("player" + this.id).innerHTML = this.name;
   }
 }
 
-function nextPlayer() {
-  player(activePlayer).setActivePlayer = true;
-}
-function init() {
-  let player1 = new Player(1, "Göran", false);
-  let player2 = new Player(2, "Bengt", false);
-  let player3 = new Player(3, "Gunhild", false);
-  let player4 = new Player(4, "Sven-Bertil", false);
+let player1 = new Player(1, "Göran");
+let player2 = new Player(2, "Bengt");
+let player3 = new Player(3, "Gunhild");
+let player4 = new Player(4, "Sven-Bertil");
 
-  let dice1 = new Dice(1, "dice1");
-  let dice2 = new Dice(2, "dice2");
-  let dice3 = new Dice(3, "dice3");
-  let dice4 = new Dice(4, "dice4");
-  let dice5 = new Dice(5, "dice5");
-  let dice6 = new Dice(6, "dice6");
-  let activePlayer = 1;
+let playersArray = [player1, player2, player3, player4];
+
+let dice1 = new Dice(1, "dice1");
+let dice2 = new Dice(2, "dice2");
+let dice3 = new Dice(3, "dice3");
+let dice4 = new Dice(4, "dice4");
+let dice5 = new Dice(5, "dice5");
+let dice6 = new Dice(6, "dice6");
+let activePlayer = 1;
+
+let td = document.getElementsByTagName("td");
+let tdList1 = [];
+let tdList2 = [];
+let tdList3 = [];
+let tdList4 = [];
+let tdList = [tdList1, tdList2, tdList3, tdList4];
+for (let i = 0; i < td.length; i++) {
+  const element = td[i];
+  if (element.id.indexOf("1") >= 0) {
+    tdList1.push(element);
+  }
+  if (element.id.indexOf("2") >= 0) {
+    tdList2.push(element);
+  }
+  if (element.id.indexOf("3") >= 0) {
+    tdList3.push(element);
+  }
+  if (element.id.indexOf("4") >= 0) {
+    tdList4.push(element);
+  }
+}
+
+function nextPlayer() {
+  let lastPlayer = "player" + activePlayer;
+  let lastPlayerArray = "tdList" + activePlayer;
+  activePlayer++;
+  activePlayer > 4 ? (activePlayer -= 4) : "";
+  let player = "player" + activePlayer;
+  let tdList = "tdList" + activePlayer;
+  document.getElementById("ones" + activePlayer).innerHTML = 75;
+  document.getElementsByTagName("td");
+  for (let i = 0; i < tdList.length; i++) {
+    const element = tdList[i];
+    console.log(element);
+  }
+}
+
+function init() {
   nextPlayer();
 }
 
@@ -72,6 +108,28 @@ function rollAllDice() {
   dice4.rollDice();
   dice5.rollDice();
   dice6.rollDice();
+}
+
+function calcSum() {
+  let input1 = document.getElementById("ones1");
+  let input2 = document.getElementById("twos1");
+  let input3 = document.getElementById("threes1");
+  let input4 = document.getElementById("fours1");
+  let input5 = document.getElementById("fives1");
+  let input6 = document.getElementById("sixes1");
+
+  let sum =
+    parseInt(input1.innerHTML) +
+    parseInt(input2.innerHTML) +
+    parseInt(input3.innerHTML) +
+    parseInt(input4.innerHTML) +
+    parseInt(input5.innerHTML) +
+    parseInt(input6.innerHTML);
+  console.log(sum);
+  document.getElementById("sum1").innerHTML = sum;
+  if (sum > 63) {
+    document.getElementById("bonus1").innerHTML = 50;
+  }
 }
 
 init();
